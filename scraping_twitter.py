29979105658c0563509510ee650c39edf97a3d1d
Non-Face <no.place.to.hide.0729@gmail.com>
2018-07-08 15:37:38 +0900

    Getting access credential from another file.

--- scraping_twitter.py
+++ scraping_twitter.py
@@ -1,1 +1,16 @@
-import requests 
+import tweepy, os
+from dotenv import load_dotenv
+from os.path import join, dirname
+
+dotenv_path=join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+
+consumer_key=os.environ.get('CONSUMER_KEY')
+consumer_secret=os.environ.get('CONSUMER_SECRET')
+access_token=os.environ.get('ACCESS_TOKEN')
+access_token_secret=os.environ.get('ACCESS_TOKEN_SECRET')
+
+#認証情報を設定する
+auth=tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
